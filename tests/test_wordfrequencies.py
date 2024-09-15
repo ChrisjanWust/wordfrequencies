9aa5c3975f2d9631e8b7abd47688a336bf073cb8
Chrisjan Wüst <chrisjanwust@gmail.com>
2024-09-15 12:20:05 +0200

    use std lib unittests

--- tests/test_wordfrequencies.py
+++ tests/test_wordfrequencies.py
@@ -1,12 +1,14 @@
+import unittest
 from wordfrequencies import get_part_of_speech, get_word_rank
 
-def test_word_frequency():
-    assert get_word_rank("the") < get_word_rank("man") < get_word_rank("jumps")
-    assert get_part_of_speech("the") == "FUNCTION_WORD"
-    assert get_part_of_speech("man") == "NOUN"
-    assert get_part_of_speech("jumps") == "VERB"
-    assert get_part_of_speech("Chrisjan") is None
-
+class TestWordFrequency(unittest.TestCase):
+    def test_word_frequency(self):
+        self.assertLess(get_word_rank("the"), get_word_rank("man"))
+        self.assertLess(get_word_rank("man"), get_word_rank("jumps"))
+        self.assertEqual(get_part_of_speech("the"), "FUNCTION_WORD")
+        self.assertEqual(get_part_of_speech("man"), "NOUN")
+        self.assertEqual(get_part_of_speech("jumps"), "VERB")
+        self.assertIsNone(get_part_of_speech("Chrisjan"))
 
 if __name__ == "__main__":
-    test_word_frequency()
+    unittest.main()
